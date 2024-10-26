392fe160c003c433bb16996a854c2cf7a21e2258
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2024-10-26 09:49:27 +0530

    Update GoalSetting.js

--- frontend/src/components/GoalSetting.js
+++ frontend/src/components/GoalSetting.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { getUserGoals } from '../api/goalapi';
 
-const API_URL = `http://localhost:4000`;
+const API_URL = `https://fitnesstracker-2.onrender.com`;
 
 const GoalSetting = () => {
     const [goal, setGoal] = useState('');
