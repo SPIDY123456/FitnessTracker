80145b76ae76f59c10023d4e04f3f25016b0021e
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2024-10-26 11:39:31 +0530

    Update goalapi.js

--- frontend/src/api/goalapi.js
+++ frontend/src/api/goalapi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `https://fitnesstracker-6.onrender.com`;
+const API_URL = `https://fitnesstracker-6.onrender.com/api/goal`;
 
 const getToken = () => localStorage.getItem('token');
 
