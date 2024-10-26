5cab3da88401e78cc0a10b62b6853601d74f33dc
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2024-10-26 09:51:10 +0530

    Update Dashboard.js

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -15,15 +15,14 @@ const Dashboard = () => {
 
     return (
         <div className="flex"> 
-            {/* Sidebar */}
             <div className="w-1/6 h-fullscreen -ml-3 -mt-4 bg-stone-800 p-4 mr-2 text-white flex flex-col items-center">
                 <button onClick={goToProfile} className="flex flex-col items-center mt-4 hover:text-stone-300">
-                    <FaUser className="text-3xl mr-40 mt-36" /> {/* Profile icon */}
-                    <span className="text-lg mr-12 -mt-8">Profile</span> {/* Profile label */}
+                    <FaUser className="text-3xl mr-40 mt-36" /> 
+                    <span className="text-lg mr-12 -mt-8">Profile</span>
                 </button>
             </div>
 
-            {/* Main Content */}
+          
             <div className="container mx-auto p-4 w-4/5">
                 <h1 className="text-4xl font-bold text-white ml-24 mb-10">Dashboard</h1>
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
